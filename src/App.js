66aa666fb4fb6246c953eba4bc0ca0d85b055170
Steven <stzheng716@gmail.com>
2023-06-01 10:07:40 -0700

    add plan for auth

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import NavBar from './NavBar';
  */
 function App() {
   //double check if user should be state
-  const [user, setUser] = useState(null)
+  const [currUser, setUser] = useState(null)
 
   //add context for use 
   function handleLogIn(){
@@ -46,3 +46,32 @@ function App() {
 }
 
 export default App;
+
+//Sudo code 
+
+//make login form 
+  //get username and password
+
+  //handleLogin function to call the static method from api.js
+
+  //handleSignUp function to call another static method from api.js for registering a user
+  
+//send username and password to backend (POST request to the backend)
+  //create a function in api.js to make POST request
+    //set token in api.js
+    //returns token to app.js
+
+//receive token back from backend and set state for token in app.js
+
+//useEffect to see if token changed.
+  //take the token and decode the payload to get username
+    //find decoding frontend JWT library (JWT-decode) 
+  //take username and make a GET to backend for user information
+    //route = /:username
+    //token be dependency of useEffect
+    //receive back: { username, firstName, lastName, isAdmin, jobs }
+
+//set state of current user to user information
+  //set the context for the user information
+
+//use logic in our component to check the context to see if user is logged in
